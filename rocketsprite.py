41b621f9be6efe1bc5813bfb37e5ace7f01b527f
FfionLee <120313877+FfionLee@users.noreply.github.com>
2025-01-11 11:27:56 +0000

    Update rocketsprite.py

--- rocketsprite.py
+++ rocketsprite.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 screen=pygame.display.set_mode((600,600))
@@ -15,17 +16,30 @@ class Player(pygame.sprite.Sprite):
     
     def update(self,keypressed):
         if keypressed[pygame.K_UP]:
-            self.rect.move_ip(0,-5)
+            self.rect.move_ip(0,-2)
         if keypressed[pygame.K_DOWN]:
-            self.rect.move_ip(0,5)
+            self.rect.move_ip(0,2)
         if keypressed[pygame.K_LEFT]:
-            self.rect.move_ip(-5,0)
+            self.rect.move_ip(-2,0)
         if keypressed[pygame.K_RIGHT]:
-            self.rect.move_ip(5,0)
+            self.rect.move_ip(2,0)
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image=pygame.image.load('meteor.png')
+        self.rect=self.image.get_rect()
+    def update(self):
+        self.rect.x=random.randint(0,600)
+        self.rect.y=random.randint(0,600)
+
 
 sprites=pygame.sprite.Group()
+obstaclegroup=pygame.sprite.Group()
 
 def startgame():
+    meteor=Obstacle()
+    obstaclegroup.add(meteor)
     rocket=Player()
     sprites.add(rocket)
     while True:
@@ -37,6 +51,8 @@ def startgame():
         rocket.update(keypressed)
         screen.blit(bg,(0,0))
         sprites.draw(screen)
+        obstaclegroup.draw(screen)
+        meteor.update()
         
         pygame.display.update()
 
